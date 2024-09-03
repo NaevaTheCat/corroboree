6f6a722c523edad4c5bb457260b4e29dc29138b6
Rachael Oxley <rachael@lowselfdiscipline.net>
2024-09-03 15:23:26 +1000

    Add basic models for policy pages
    Add templates for policies and fixes to models

--- corroboree/models.py
+++ corroboree/models.py
@@ -6,11 +6,54 @@ from wagtail import blocks
 from wagtail.snippets.blocks import SnippetChooserBlock
 from corroboree.config.models import Season, BookingType
 
+
 class ContactPage(Page):
     pass
 
+
+class PolicyPage(Page):
+    date_revised = models.DateField()
+    body = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date_revised'),
+        FieldPanel('body'),
+    ]
+
+    parent_page_types = ['PoliciesPage', 'PolicyPage']
+    subpage_types = ['PolicyPage']
+
+
+class SectionBlock(blocks.StructBlock):
+    heading = blocks.CharBlock(max_length=512)
+    intro_text = blocks.RichTextBlock(required=False)
+
+
+class PolicyWithSubPoliciesBlock(blocks.StructBlock):
+    policy = blocks.PageChooserBlock(page_type=PolicyPage)
+    sub_policies = blocks.ListBlock(
+        blocks.PageChooserBlock(page_type=PolicyPage),
+    )
+
+
 class PoliciesPage(Page):
-    pass
+    subheading = models.CharField(max_length=512)
+    introduction = RichTextField()
+    body = StreamField([
+        ('policy', blocks.PageChooserBlock(page_type=PolicyPage)),
+        ('section', SectionBlock()),
+        ('policy_with_subpolicies', PolicyWithSubPoliciesBlock()),
+    ], blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subheading'),
+        FieldPanel('introduction'),
+        FieldPanel('body'),
+    ]
+
+    parent_page_types = ['home.HomePage']
+    subpage_types = ['PolicyPage']
+
 
 class TextPage(Page):
     body = RichTextField()
