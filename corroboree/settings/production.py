84e30514284fc53cec720fd63228ecca01f34072
Rachael Oxley <rachael@lowselfdiscipline.net>
2024-12-09 14:37:33 +1100

    smtp email settings in prod settings

--- corroboree/settings/production.py
+++ corroboree/settings/production.py
@@ -1,6 +1,27 @@
 from .base import *
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+
 DEBUG = False
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# Email SMTP settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
+EMAIL_SSL_KEYFILE = os.getenv('EMAIL_SSL_KEYFILE')
+EMAIL_SSL_CERTFILE = os.getenv('EMAIL_SSL_CERTFILE')
 
 try:
     from .local import *
